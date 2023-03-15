981767b2fd360884ba8a9294f453ad281262bbc0
brucewzj99 <bruceloveapple@gmail.com>
2023-03-15 11:22:17 +0800

    read from file instead

--- frontend/src/components/PageShell.js
+++ frontend/src/components/PageShell.js
@@ -1,106 +1,14 @@
 import { Card, Title, Text, Tab, TabList, Grid } from "@tremor/react";
-
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CompanyCard from "./CompanyCard";
 import Donut from "./ReviewDonut";
+import companyData from "../assets/test.json"
 
 // yes we must format the sentiment in this way due to
 // how donut chart takes in data
 
 export default function PageShell() {
-    const [companyData, updateCompanyData] = useState([
-        {
-            name: "Meta",
-            desc: "Hi I am Meta",
-            overallSentiment: [
-                {
-                    type: "positive",
-                    sentiment: 480
-                },
-                {
-                    type: "negative",
-                    sentiment: 80
-                },
-                {
-                    type: "neutral",
-                    sentiment: 130
-                }
-            ]
-        },
-        {
-            name: "Amazon",
-            desc: "Hi I am Amazon",
-            overallSentiment: [
-                {
-                    type: "positive",
-                    sentiment: 231
-                },
-                {
-                    type: "negative",
-                    sentiment: 64
-                },
-                {
-                    type: "neutral",
-                    sentiment: 38
-                }
-            ]
-        },
-        {
-            name: "Apple",
-            desc: "Hi I am Apple",
-            overallSentiment: [
-                {
-                    type: "positive",
-                    sentiment: 657
-                },
-                {
-                    type: "negative",
-                    sentiment: 167
-                },
-                {
-                    type: "neutral",
-                    sentiment: 31
-                }
-            ]
-        },
-        {
-            name: "Netflix",
-            desc: "Hi I am Netflix",
-            overallSentiment: [
-                {
-                    type: "positive",
-                    sentiment: 161
-                },
-                {
-                    type: "negative",
-                    sentiment: 321
-                },
-                {
-                    type: "neutral",
-                    sentiment: 277
-                }
-            ]
-        },
-        {
-            name: "Google",
-            desc: "Hi I am Google",
-            overallSentiment: [
-                {
-                    type: "positive",
-                    sentiment: 385
-                },
-                {
-                    type: "negative",
-                    sentiment: 351
-                },
-                {
-                    type: "neutral",
-                    sentiment: 281
-                }
-            ]
-        }
-    ]);
-        
+ 
   const [selectedView, setSelectedView] = useState("1");
   return (
     <main>
@@ -125,6 +33,7 @@ export default function PageShell() {
 
       {selectedView === "1" ? (
         <>
+        
             {/* Main section */}
             <Card className="mt-6">
                 <CompanyCard
@@ -146,6 +55,7 @@ export default function PageShell() {
         <Card className="mt-6">
             <Grid numColsSm={2} numColsLg={3} className="gap-6">
                 {
+                    
                     companyData.map((company) => (
                         <Donut
                             key={company.name}
