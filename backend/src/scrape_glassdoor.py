459e27dce10292bd49f2ecf5e7db9745e5b82d70
Juleus <93301912+FS75@users.noreply.github.com>
2023-03-31 16:54:44 +0800

    Update scrape_glassdoor.py

--- backend/src/scrape_glassdoor.py
+++ backend/src/scrape_glassdoor.py
@@ -12,40 +12,14 @@ import pandas as pd
 from main import reviews_to_scrape, details_folder_prefix, review_folder_prefix
 
 user_agent_list = [
-    # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36",
-    # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
-    # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+        # "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36",
+        # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36",
+        # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36",
+        # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
     ]
-
-country_code = {
-    "Argentina": 13,
-    "Australia": 5,
-    "Belgique (Français)": 15,
-    "België (Nederlands)": 14,
-    "Brasil": 9,
-    "Canada (English)": 3,
-    "Canada (Français)": 19,
-    "Deutschland": 7,
-    "España": 8,
-    "France": 6,
-    "Hong Kong": 20,
-    "India": 4,
-    "Ireland": 18,
-    "Italia": 23,
-    "México": 12,
-    "Nederland": 10,
-    "New Zealand": 21,
-    "Schweiz (Deutsch)": 16,
-    "Singapore": 22,
-    "Suisse (Français)": 17,
-    "United Kingdom": 2,
-    "United States": 1,
-    "Österreich": 11
-}
 
 # "HSBC", "United Overseas Bank", "Micron Technology, Inc",
 # "Meta", "Amazon", "Apple",
@@ -53,11 +27,7 @@ country_code = {
 # "Google", "Infineon Technologies", "St Engineering",
 # "Sembcorp", "Ntuc Fairprice"
 
-# "HSBC", "Micron Technology, Inc",
-# "Amazon", "Apple",
-# "Dbs Bank", "Accenture", "Netflix",
-# "Google", "Infineon Technologies",
-companies_to_scrape = ["HSBC"]
+companies_to_scrape = ["Amazon"]
 
 pandas_glassdoor_details_dict = {
     "employer_name": [],
@@ -106,86 +76,6 @@ async def scrape_glassdoor():
         pandas_dict_to_csv(pandas_glassdoor_reviews_dict, f"{review_folder_prefix}/{company}_glassdoor_review.csv")
 
 
-"""
-Purpose: Function to generate a response for specified URL
-PARAMS: url : string - contains URL to a website 
-        eg. "https://www.glassdoor.com/Overview/Working-at-eBay-EI_IE7853.11,15.htm"
-"""
-
-
-async def get_response(url):
-    timeout = httpx.Timeout(30.0, read=120.0)
-    client = httpx.AsyncClient(timeout=timeout)
-    res = await client.get(
-        url,
-        cookies={'tldp': f'{random.randint(1, 10000)}'},
-        follow_redirects=True,
-        headers={'User-Agent': random.choice(user_agent_list)}
-    )
-    return res
-
-
-"""
-Purpose: Function to scrape glassdoor and get the payload with a specified response
-PARAMS: response - contains entire page's CSS which will be read with Selector
-"""
-
-
-def get_glassdoor_details_payload(company_payload, response):
-    selector = Selector(response.text)
-    return {
-        "employer_name": company_payload["suggestion"],
-        "employer_id": company_payload["id"],
-        "employer_website": selector.css('[data-test="employer-website"]::text').get(),
-        "employer_description": selector.css('[data-test="employerDescription"]::text').get(),
-        "employer_size": selector.css('[data-test="employer-size"]::text').get(),
-        "employer_revenue": selector.css('[data-test="employer-revenue"]::text').get(),
-        "employer_headquarters": selector.css('[data-test="employer-headquarters"]::text').get(),
-        "employer_founded": selector.css('[data-test="employer-founded"]::text').get(),
-        "employer_industry": selector.css('[data-test="employer-industry"]::text').get()
-    }
-
-
-def glassdoor_find_companies(query: str):
-    """find company Glassdoor ID and name by query. e.g. "ebay" will return "eBay" with ID 7853"""
-    result = httpx.get(
-        url=f"https://www.glassdoor.com/searchsuggest/typeahead?numSuggestions=8&source=GD_V2&version=NEW&rf=full&fallback=token&input={query}",
-        headers={'User-Agent': random.choice(user_agent_list)}
-    )
-    # print(result.content)
-    data = json.loads(result.content)
-    return {
-        "suggestion": data[0]["suggestion"],
-        "id": data[0]["employerId"]
-    }
-
-
-def details_payload_to_pandas_dict(payload, pandas_dict):
-    # value is dict[key]
-    for key in payload:
-        pandas_dict[key].append(payload[key])
-
-
-def review_payload_to_pandas_dict(payload, pandas_dict):
-    for key in payload:
-        for item in payload[key]:
-            pandas_dict[key].append(item)
-
-
-def pandas_dict_to_csv(pandas_dict, filename):
-    key_lengths = []
-    keys = ["Rating", "Title", "Pro", "Con", "Job Title", "Status", "Region", "Date"]
-    for key in pandas_dict:
-        key_lengths.append(len(pandas_dict[key]))
-
-    for i in range(0, 8):
-        while pandas_dict[keys[i]] <= max(key_lengths):
-            pandas_dict[keys[i]].append("")
-
-    df = pd.DataFrame(pandas_dict)
-    df.to_csv(filename)
-
-
 async def get_glassdoor_reviews(company_payload):
     first_page = await get_response(
         url=f"https://www.glassdoor.com/Reviews/"
@@ -193,7 +83,7 @@ async def get_glassdoor_reviews(company_payload):
             f"{company_payload['id']}_P1.htm"
     )
 
-    await asyncio.sleep(1)
+    # await asyncio.sleep(1)
 
     reviews_payload = {
         "Rating": [],
@@ -220,24 +110,37 @@ async def get_glassdoor_reviews(company_payload):
 
     print(f"Commencing scraping of {actual_pages_to_scrape} pages for {company_payload['suggestion']}'s reviews")
 
-    for page in range(1, actual_pages_to_scrape + 1):
+    for page in range(1, 251):
         print(f"Scraping {company_payload['suggestion']}'s reviews from page {page}")
         response = await get_response(f"https://www.glassdoor.com/Reviews/"
                                 f"{company_payload['suggestion']}-Reviews-E"
                                 f"{company_payload['id']}_P"
                                 f"{page}.htm")
-        await asyncio.sleep(1)
+        # await asyncio.sleep(0.5)
         glassdoor_reviews_payload = get_glassdoor_reviews_payload(response)
         for key in glassdoor_reviews_payload:
             for item in glassdoor_reviews_payload[key]:
                 reviews_payload[key].append(item)
 
-            # to ensure same rows for pandas to convert to df
-            # while len(reviews_payload[key]) <= 10:
-            # #     # print(reviews_payload[key])
-            #     reviews_payload[key].append("")
-
     return reviews_payload
+
+"""
+Purpose: Function to generate a response for specified URL
+PARAMS: url : string - contains URL to a website 
+        eg. "https://www.glassdoor.com/Overview/Working-at-eBay-EI_IE7853.11,15.htm"
+"""
+
+
+async def get_response(url):
+    timeout = httpx.Timeout(120.0, read=120.0)
+    client = httpx.AsyncClient(timeout=timeout)
+    res = await client.get(
+        url,
+        cookies={'tldp': f'{random.randint(1, 10000)}'},
+        follow_redirects=True,
+        headers={'User-Agent': random.choice(user_agent_list)}
+    )
+    return res
 
 
 def get_glassdoor_reviews_payload(response):
@@ -273,6 +176,32 @@ def get_glassdoor_reviews_payload(response):
         "Region": region_holder,
         "Date": date_holder
     }
+
+
+def details_payload_to_pandas_dict(payload, pandas_dict):
+    # value is dict[key]
+    for key in payload:
+        pandas_dict[key].append(payload[key])
+
+
+def review_payload_to_pandas_dict(payload, pandas_dict):
+    for key in payload:
+        for item in payload[key]:
+            pandas_dict[key].append(item)
+
+
+def pandas_dict_to_csv(pandas_dict, filename):
+    key_lengths = []
+    keys = ["Rating", "Title", "Pro", "Con", "Job Title", "Status", "Region", "Date"]
+    for key in pandas_dict:
+        key_lengths.append(len(pandas_dict[key]))
+
+    for i in range(0, 8):
+        while len(pandas_dict[keys[i]]) <= max(key_lengths):
+            pandas_dict[keys[i]].append("")
+
+    df = pd.DataFrame(pandas_dict)
+    df.to_csv(filename)
 
 
 def extract_apollo_state(html):
@@ -312,3 +241,38 @@ def extract_job_details(string):
 def extract_status(string):
     if string.find(",") != 1:
         return string[:string.find(",")]
+
+
+"""
+Purpose: Function to scrape glassdoor and get the payload with a specified response
+PARAMS: response - contains entire page's CSS which will be read with Selector
+"""
+
+
+def get_glassdoor_details_payload(company_payload, response):
+    selector = Selector(response.text)
+    return {
+        "employer_name": company_payload["suggestion"],
+        "employer_id": company_payload["id"],
+        "employer_website": selector.css('[data-test="employer-website"]::text').get(),
+        "employer_description": selector.css('[data-test="employerDescription"]::text').get(),
+        "employer_size": selector.css('[data-test="employer-size"]::text').get(),
+        "employer_revenue": selector.css('[data-test="employer-revenue"]::text').get(),
+        "employer_headquarters": selector.css('[data-test="employer-headquarters"]::text').get(),
+        "employer_founded": selector.css('[data-test="employer-founded"]::text').get(),
+        "employer_industry": selector.css('[data-test="employer-industry"]::text').get()
+    }
+
+
+def glassdoor_find_companies(query: str):
+    """find company Glassdoor ID and name by query. e.g. "ebay" will return "eBay" with ID 7853"""
+    result = httpx.get(
+        url=f"https://www.glassdoor.com/searchsuggest/typeahead?numSuggestions=8&source=GD_V2&version=NEW&rf=full&fallback=token&input={query}",
+        headers={'User-Agent': random.choice(user_agent_list)}
+    )
+    # print(result.content)
+    data = json.loads(result.content)
+    return {
+        "suggestion": data[0]["suggestion"],
+        "id": data[0]["employerId"]
+    }
